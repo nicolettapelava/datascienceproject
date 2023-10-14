7ed7deaa0feb758259762bc42e0ce8ed44884152
nicolettapelava <147842789+nicolettapelava@users.noreply.github.com>
2023-10-14 19:36:25 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# * learn about code asset management
+# * learn about data asset management
+# * hands on with jupyter note, github & r
+
+# Some of the popular languages that Data Scientists use are:
+# 1. python
+# 2. r
+# 3. sas e miner
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. scikit-learn
+# 2. pandas
+# 3. numpy
+# 4. keras
+
+# |Data Science Tools|
+# |------------------|
+# |github|
+# |ibm watson studio|
+# |google collab|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# this a simple arithmetic expression to mutiply then add integers.
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60.
+
+# In[2]:
+
+
+200/60
+
+
+# ## Author
+# nicoletta
+
+# In[ ]:
+
+
+
+
